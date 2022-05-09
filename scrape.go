bbf7dd327c9145f4490986c3aef318f558f8d35f
Chems-Eddine NAIMI <81744426+Chems-bitti@users.noreply.github.com>
2022-05-09 17:31:33 +0200

    Added explanatory comments

--- scrape.go
+++ scrape.go
@@ -13,35 +13,42 @@ import (
 )
 
 func main() {
-	jobnum := 0
-	fName := os.Args[1]
-	file, err := os.Create(fName)
+	jobnum := 0 // This will be our total jobs scraped counter
+	fName := os.Args[1] // reading the file name given, should be data.csv if you use my script
+	// I don't check whether it's data.csv or not just in case you want to use your own script to expand 
+	file, err := os.Create(fName) 
 	if err != nil {
 		log.Fatalf("Could not create file, err: %q", err)
 		return
 	}
-	defer file.Close()
+	defer file.Close() // This is so handy coming from C
 
-	writer := csv.NewWriter(file)
+	writer := csv.NewWriter(file) 
 	defer writer.Flush()
-	c := colly.NewCollector()
+	c := colly.NewCollector() // Creating a new collector
+	// Now this part is only here in case I want to add delays later, for now they work with 0 delay, if you ever get 
+	// "Too many requests" error message, try adding a delay (1 second for example)
 	c.Limit(&colly.LimitRule{
-		Delay:       0 * time.Second,
+		Delay:       0 * time.Second, 
 		RandomDelay: 0 * time.Second,
 	})
-	c.SetRequestTimeout(20 * time.Second)
+	c.SetRequestTimeout(20 * time.Second) // Almost never happens if you have internet 
 	c.IgnoreRobotsTxt = true
 	extensions.RandomUserAgent(c)
 
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		i := 0
-		e.ForEach("div[data-entity-urn]", func(_ int, el *colly.HTMLElement) {
-			time := el.ChildText("time")
-			id := el.Attr("data-entity-urn")
-			name := el.ChildText("h3")
+		// Now for this part, the collector looks for a div that has the data-entity-urn attribute
+		// When it finds it, it scrapes the contents of its children
+		e.ForEach("div[data-entity-urn]", func(_ int, el *colly.HTMLElement) { 
+			time := el.ChildText("time") // not used really but might come in handy later
+			id := el.Attr("data-entity-urn") // The id of the job listing, used for the link
+			name := el.ChildText("h3") // A more appropriate name should be title but eh
 			company := el.ChildText("h4")
 			location := el.ChildText("span.job-search-card__location")
-			if len(id) != 0 && len(time) != 0 && len(company) != 0 && len(name) != 0 && len(location) != 0 {
+			// Sometimes it detects empty lines (I have no idea why), so this if statement saves us from the panic that insues
+			// due to `id[18:]
+			if len(id) != 0 && len(time) != 0 && len(company) != 0 && len(name) != 0 && len(location) != 0 { 
 				if strings.Contains(time, "minute") || time == "1 hour ago" {
 					link := "https://www.linkedin.com/jobs/view/" + id[18:] + " "
 					writer.Write([]string{
@@ -58,7 +65,7 @@ func main() {
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
-
+	// Check the picture in README.md in order to know what to do here
 	c.Visit("link1")
 	c.Visit("link2")
 	c.Visit("link3")
